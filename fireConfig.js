0fac3bf043303eb72824f74bc570d1af922ffcc0
Gabriel1353 <163824883+Gabriel1353@users.noreply.github.com>
2024-05-27 15:00:06 +0800

    Initial Commit

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDQnMGEPrBmULUAaiBaHQraF8lBzlqpPpc",
+  authDomain: "crud-abc6c.firebaseapp.com",
+  databaseURL: "https://crud-abc6c-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "crud-abc6c",
+  storageBucket: "crud-abc6c.appspot.com",
+  messagingSenderId: "465291120222",
+  appId: "1:465291120222:web:71b2740e6c22565c1d2ecf"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
